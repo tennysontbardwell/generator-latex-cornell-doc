7da9cc1a595f15695fbe8d5163c2758693c7cc68
Tennyson Taylor Bardwell <tennysontaylorbardwell@gmail.com>
2016-09-27 01:25:32 -0400

    inital commit, working generator

---
+++ README.md
@@ -0,0 +1,36 @@
+# generator-latex-cornell-doc [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> A document for cornell related school work.
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-latex-cornell-doc using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-latex-cornell-doc
+```
+
+Then generate your new project:
+
+```bash
+yo latex-cornell-doc
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+ © [Tennyson T Bardwell]()
+
+
+[npm-image]: https://badge.fury.io/js/generator-latex-cornell-doc.svg
+[npm-url]: https://npmjs.org/package/generator-latex-cornell-doc
+[travis-image]: https://travis-ci.org/tennysontbardwell/generator-latex-cornell-doc.svg?branch=master
+[travis-url]: https://travis-ci.org/tennysontbardwell/generator-latex-cornell-doc
+[daviddm-image]: https://david-dm.org/tennysontbardwell/generator-latex-cornell-doc.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/tennysontbardwell/generator-latex-cornell-doc
---
+++ generators/app/index.js
@@ -0,0 +1,96 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.Base.extend({
+  prompting: function () {
+    var prompts = [
+      {
+        type: 'input',
+        name: 'filename',
+        message: 'What would you like the filename to be (no extension)?',
+        default: 'assignment'
+      },
+      {
+        type: 'rawlist',
+        name: 'couse',
+        message: 'What class is this for?',
+        choices: ['CS 3110', 'ECE 2300', 'CS 5414']
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of this document?'
+      },
+      {
+        type: 'input',
+        name: 'cover_author',
+        message: 'What should the name on the cover be?',
+        default: 'Tennyson T \\textsc{Bardwell}\\\\(ttb33)'
+      },
+      {
+        type: 'input',
+        name: 'header_author',
+        message: 'What should the name on the header be?',
+        default: 'Tennyson T Bardwell (ttb33)'
+      }
+    ];
+
+    return this.prompt(prompts).then(function (props) {
+      this.props = props;
+      return props;
+    }.bind(this));
+  },
+
+  writing: function () {
+    this.fs.copyTpl(
+      this.templatePath('Makefile'),
+      this.destinationPath('Makefile'),
+      {filename: this.props.filename}
+    );
+    this.fs.copy(
+      this.templatePath('cover_page.tex'),
+      this.destinationPath('cover_page.tex')
+    );
+    this.fs.copy(
+      this.templatePath('images/cu.png'),
+      this.destinationPath('images/cu.png')
+    );
+    this.fs.copy(
+      this.templatePath('template.tex'),
+      this.destinationPath('template.tex')
+    );
+    var couse = {};
+    if (this.props.couse === 'CS 5414') {
+      couse.course_code = 'CS 5414';
+      couse.couse_name = 'Principles of Distributed Computing';
+      couse.professor_name = 'Lorenzo Alvisi';
+    }
+    if (this.props.couse === 'CS 3110') {
+      couse.course_code = 'CS 3110';
+      couse.couse_name = 'Data Structures and Functional Programming';
+      couse.professor_name = 'Michael \\textsc{Clarkson}';
+    }
+    if (this.props.couse === 'ECE 2300') {
+      couse.course_code = 'ECE 2300';
+      couse.couse_name = 'Digital Logic and Computer Organization';
+      couse.professor_name = 'David \\textsc{Albonesi}';
+    }
+    this.fs.copyTpl(
+      this.templatePath('main_file.tex'),
+      this.destinationPath(this.props.filename + '.tex'),
+      {
+        cover_author: this.props.cover_author,
+        header_author: this.props.header_author,
+        title: this.props.title,
+        course_code: couse.course_code,
+        course_name: couse.couse_name,
+        professor_name: couse.professor_name
+      }
+    );
+  },
+
+  install: function () {
+  }
+});
